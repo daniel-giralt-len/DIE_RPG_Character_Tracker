d7bb32dc47e57d586d0401efe173aa5080fceb33
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-26 16:46:09 +0200

    use font minion pro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import Stat from './Components/Stat'
+import { createGlobalStyle } from 'styled-components'
 
-function App() {
-  return (
-    <div>
-      <Stat value={4} paragon='dictator' />
-      <Stat value={4} paragon='fool' />
-      <Stat value={4} paragon='emotion knight' />
-      <Stat value={4} paragon='neo' />
-      <Stat value={4} paragon='godbinder' />
-      <Stat value={4} paragon='master' />
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Minion Pro";
+    src: url(MinionPro-Medium.otf) format("opentype");
+    font-weight: normal;
+  }
+  @font-face {
+    font-family: "Minion Pro";
+    src: url(MinionPro-BoldCnCapt.otf) format("opentype");
+    font-weight: bold;
+  }
+  html, button, input, textarea {
+    font-family: 'Minion Pro', sans-serif;
+    font-size: 3em;
+  }
+`;
+
+const App = () => (
+  <>
+    <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,500;0,700;0,900;1,200;1,500;1,700&display=swap" rel="stylesheet"></link>
+    <GlobalStyle/>
+      <div>
+        <div>Font Testing String</div>
+        <Stat value={4} paragon='dictator' />
+        <Stat value={4} paragon='fool' />
+        <Stat value={4} paragon='emotion knight' />
+        <Stat value={4} paragon='neo' />
+        <Stat value={4} paragon='godbinder' />
+        <Stat value={4} paragon='master' />
+      </div>
+    </>
+ );
 
 export default App;
