151c11c3ccf101c8e136a77045beece083888e1a
daniel-giralt-len <daniel.giralt.len@gmail.com>
2022-10-27 14:55:54 +0200

    display initial advancements on wizard

---
+++ src/data/advancementsDb.js
@@ -0,0 +1,17 @@
+import dictatorAdvancement from './dictatorAdvancement'
+import emotionKnightAdvancement from './emotionKnightAdvancement'
+import foolAdvancement from './foolAdvancement'
+import godbinderAdvancement from './godbinderAdvancement'
+import masterAdvancement from './masterAdvancement'
+import neoAdvancement from './neoAdvancement'
+
+const advancementsDb = {
+    dictator: dictatorAdvancement,
+    emotionKnight: emotionKnightAdvancement,
+    fool: foolAdvancement,
+    godbinder: godbinderAdvancement,
+    master: masterAdvancement,
+    neo: neoAdvancement,
+}
+
+export default advancementsDb
